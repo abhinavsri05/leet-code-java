e0d83511c50621d6cf34e9e40c9da0f752d291e3
Abhinav Srivastava <abhinavsri@gmail.com>
2021-12-26 22:32:06 -0500

    Added

---
+++ src/main/java/n03_graphs/P00733FloodFill.java
@@ -0,0 +1,38 @@
+package n03_graphs;
+
+public class P00733FloodFill {
+    private void floodFillImage(int[][] image, int sr, int sc, int origColor, int newColor)
+    {
+
+        if (image[sr][sc] == newColor || image[sr][sc] != origColor)
+            return;
+
+        image[sr][sc] = newColor;
+
+        // up pixel
+        if (sr - 1 >= 0)
+            floodFillImage(image, sr - 1, sc, origColor, newColor);
+
+        // down pixel
+        if (sr + 1 < image.length)
+            floodFillImage(image, sr + 1, sc, origColor, newColor);
+
+        // left pixel
+        if (sc - 1 >= 0)
+            floodFillImage(image, sr, sc - 1, origColor, newColor);
+
+        // right pixel
+        if (sc + 1 < image[0].length)
+            floodFillImage(image, sr, sc + 1, origColor, newColor);
+
+
+
+    }
+
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor)
+    {
+        int[][] newImage = image.clone();
+        floodFillImage(newImage, sr, sc, image[sr][sc], newColor);
+        return newImage;
+    }
+}

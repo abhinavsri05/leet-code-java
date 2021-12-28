876a1159024533fe4428d183cb27db362b8f80cc
Abhinav Srivastava <abhinavsri@gmail.com>
2021-12-27 22:28:20 -0500

    Added Day 10 from 14 day study plan

---
+++ src/main/java/n01_data_structures/P0021MergeTwoSortedLists.java
@@ -0,0 +1,61 @@
+package n01_data_structures;
+
+public class P0021MergeTwoSortedLists
+{
+    public class ListNode
+    {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2)
+    {
+        ListNode root, merged;
+
+        if (list1 == null)
+            return list2;
+        if (list2 == null)
+            return list1;
+
+        if (list1.val < list2.val)
+        {
+            root = list1;
+            list1 = list1.next;
+        }
+        else
+        {
+            root = list2;
+            list2 = list2.next;
+        }
+
+        merged = root;
+        merged.next = null;
+
+        while (list1 != null && list2 != null)
+        {
+            if (list1.val < list2.val)
+            {
+                merged.next = list1;
+                list1 = list1.next;
+            }
+            else
+            {
+                merged.next = list2;
+                list2 = list2.next;
+            }
+
+            merged = merged.next;
+            merged.next = null;
+        }
+
+        if (list1 == null)
+            merged.next = list2;
+
+        if (list2 == null)
+            merged.next = list1;
+
+        return root;
+    }
+}
---
+++ src/main/java/n01_data_structures/P0206ReverseLinkedList.java
@@ -0,0 +1,31 @@
+package n01_data_structures;
+
+public class P0206ReverseLinkedList {
+    public class ListNode
+    {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public ListNode reverseList(ListNode head)
+    {
+        if (head == null || head.next == null)
+            return head;
+
+        ListNode prev = null, next;
+
+        while (head != null)
+        {
+            next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+
+        return prev;
+
+    }
+}

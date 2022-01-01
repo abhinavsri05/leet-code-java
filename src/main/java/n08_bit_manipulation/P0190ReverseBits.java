933b25cb5b9beb229528e9d06367c779fdfecdeb
Abhinav Srivastava <abhinavsri@gmail.com>
2022-01-01 00:06:59 -0500

    Added Day 14/14 (Algorithms-I) study plan

---
+++ src/main/java/n08_bit_manipulation/P0190ReverseBits.java
@@ -0,0 +1,17 @@
+package n08_bit_manipulation;
+
+public class P0190ReverseBits {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        int rev = 0;
+
+        for (int pow = 31; pow >= 0; pow--)
+        {
+            rev += (n & 1) << pow;
+            n = n >> 1;
+        }
+
+        return rev;
+
+    }
+}

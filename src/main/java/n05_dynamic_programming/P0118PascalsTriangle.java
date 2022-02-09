80a4acfc6d9bc889eb8c1f1bd0f2623f967e6a6e
Abhinav Srivastava <abhinavsri@gmail.com>
2022-02-08 20:58:18 -0500

    Started DS1 Plan Day 5/14

---
+++ src/main/java/n05_dynamic_programming/P0118PascalsTriangle.java
@@ -0,0 +1,34 @@
+package n05_dynamic_programming;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class P0118PascalsTriangle
+{
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> triangle = new ArrayList<>(numRows);
+        int i, j;
+
+        List<Integer> current = new ArrayList<>(1);
+
+        current.add(0, 1);
+
+        triangle.add(0, current);
+
+        for (i = 1; i < numRows; i++)
+        {
+            current = new ArrayList<>(i + 1);
+            current.add(0, 1);
+
+            for(j = 1; j < i; j++)
+            {
+                current.add(j, triangle.get(i - 1).get(j - 1) + triangle.get(i - 1).get(j));
+            }
+
+            current.add(i, 1);
+            triangle.add(i, current);
+        }
+
+        return triangle;
+    }
+}
